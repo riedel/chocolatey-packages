f2d91f57b7aecc5af8bd9849b7bbda57744b706f
Till Riedel <riedel@teco.edu>
2020-05-24 19:29:33 +0200

    add webcamoid

---
+++ webcamoid/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageArgs = @{
+  packageName            = $Env:ChocolateyPackageName
+  fileType               = 'EXE'
+  url                    = 'https://github.com/webcamoid/webcamoid/releases/download/8.7.1/webcamoid-8.7.1-win32.exe'
+  url64                    = 'https://github.com/webcamoid/webcamoid/releases/download/8.7.1/webcamoid-8.7.1-win64.exe'
+  checksum               = '84f6fb46446e8c6ced33899070655b7de089b47ca5412db84c8913a9e3ac2bce'
+  checksum64               = '0ec1b61ca7d658d0111789bcc666f4821d19411e61c633a2c24d5d2108168d8f'
+  checksumType           = 'sha256'
+  silentArgs 		 = "--script $Env:ChocolateyPackageFolder/tools/silentInstall.js"
+  validExitCodes         = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+Register-Application "$installLocation\bin\$packageName.exe"
+Write-Host "$packageName registered as $packageName"
---
+++ webcamoid/update.ps1
@@ -0,0 +1,71 @@
+import-module au
+
+function global:au_GetLatest {
+    $github_repository = "webcamoid/webcamoid"
+
+    $package_repository= (git config remote.origin.url).split(":")[1].split(".")[0] 
+
+    $repo = Invoke-RestMethod ("https://api.github.com/repos/" + $github_repository)
+
+
+    $release = Invoke-RestMethod ("https://api.github.com/repos/" + $github_repository + "/releases/latest")     
+
+
+    $license = Invoke-RestMethod ("https://api.github.com/repos/" + $github_repository + "/license")     
+
+
+    $readme = Invoke-RestMethod ("https://api.github.com/repos/" + $github_repository + "/readme")     
+
+
+   $topics=(Invoke-RestMethod -Headers @{'Accept' = 'application/vnd.github.mercy-preview+json'} ("https://api.github.com/repos/" + $github_repository + "/topics")).names -join " "
+
+
+   $release_author = Invoke-RestMethod $release.author.url
+
+
+     @{
+        URL32 = ($release.assets | where-object {$_.name -like "*win32.exe" })[0].browser_download_url 
+        URL64 = ($release.assets | where-object {$_.name -like "*win64.exe" })[0].browser_download_url 
+	readmeUrl  = $readme.download_url
+        Version = $release.tag_name
+        packageSourceUrl   = 'https://github.com/' + $package_repository
+        projectUrl = $repo.homepage 
+        projectSourceUrl   = $repo.html_url 
+        githubRawUrl   = 'https://raw.githubusercontent.com/' + $github_repository + '/' + $release.tag_name 
+        githubUrl   = 'https://github.com/' + $github_repository + '/tree/' + $release.tag_name 
+        releaseNotes = $release.body 
+        licenseUrl = $license.html_url 
+        summary = $repo.description 
+        authors = $release_author.name 
+	tags = $topics
+       	iconPath = '/BrowserSelect/bs.ico'
+    }
+
+}
+
+
+function global:au_SearchReplace {
+@{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+        }
+}
+}
+
+function global:au_BeforeUpdate($package) {
+    "`n" + (Invoke-WebRequest -Uri $Latest.readmeUrl).Content | Out-File -Encoding "UTF8" ($package.Path + "\README.md")
+
+$package.NuspecXml.package.metadata.ChildNodes|% { $cn=$_ ; $cn.innerText|select-String '{([A-Za-z_]*)}' -AllMatches| % {$_.matches.groups} | where-object {$_.Name -eq 1} | % {$cn.InnerText = $cn.InnerText -replace ("{"+$_.Value+"}"),$Latest[$_.Value]}} 
+}
+
+function global:au_AfterUpdate($package) {
+    rm ($package.Path + "\README.md")
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    Get-ChildItem -Filter "*.in" -Recurse | Copy-Item -Destination {$_.name -replace '.in$','' }
+    update
+}
