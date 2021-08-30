01259f91124de460f91c51d878c316dca145eeac
Till Riedel <riedel@teco.edu>
2021-08-30 22:35:09 +0200

    Fix nuspec and add icon

--- SMathStudioDesktop/update.ps1
+++ SMathStudioDesktop/update.ps1
@@ -36,13 +36,16 @@ function global:au_GetLatest {
 
 	$stable[0].firstChild().firstChild().innerText -match '[0-9]*\.[0-9]*\.[0-9]*'
 	$version = $Matches[0]
+
+	$releaseNotes = $stable[0].nextSibling().innerHtml|pandoc -f html -t markdown | Out-String
+
 	
 
 	return @{
 		    readmeUrl  =  $homepage + "/view/SMathStudio/summary"
 			URL32  = $file[0].href
 			Version = $version
-			releaseNotes = $stable[0].nextSibling().innerHtml
+			releaseNotes = $releaseNotes
 			Options = @{
 				Headers = @{
 				  referer = $homepage + "/view/SMathStudio/summary"
@@ -56,7 +59,7 @@ function global:au_GetLatest {
 function global:au_BeforeUpdate($package) {
     $Latest.ChecksumType32 = 'sha256' # Not necessary, but just my preference to add it
     $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32 -Algorithm $Latest.ChecksumType32 -Headers $Latest.Options.Headers # You can omit the algorithm, the function will use sha256 by default
-	$package.NuspecXml.package.metadata.ChildNodes|ForEach-Object { $cn=$_ ; $cn.innerText|select-String '{([A-Za-z_]*)}' -AllMatches| % {$_.matches.groups} | where-object {$_.Name -eq 1} | % {$cn.InnerText = $cn.InnerText -replace ("{"+$_.Value+"}"),$Latest[$_.Value]}} 
+		$package.NuspecXml.package.metadata.ChildNodes|ForEach-Object { $cn=$_ ; $cn.innerText|select-String '{([A-Za-z_]*)}' -AllMatches| % {$_.matches.groups} | where-object {$_.Name -eq 1} | % {$cn.InnerText = $cn.InnerText -replace ("{"+$_.Value+"}"),$Latest[$_.Value]}} 
 }
 
 
@@ -76,5 +79,5 @@ function global:au_AfterUpdate($package) {
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
 	Get-ChildItem -Filter "*.in" -Recurse | Copy-Item -Destination {$_.name -replace '.in$','' }
-	update -ChecksumFor none
+	update -ChecksumFor none 
 }
